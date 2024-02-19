fa5fde505fe17cafeafbb3b21f763a8de094ad43
matthew-r-tanudjaja <matthew.r.tanudjaja@linecorp.com>
2024-02-19 21:02:39 +0700

    EMP-16 Auto-infer generic types

--- internal/account/dto.go
+++ internal/account/dto.go
@@ -3,7 +3,8 @@ package account
 import (
 	"github.com/mrexmelle/connect-emp/internal/career"
 	"github.com/mrexmelle/connect-emp/internal/dto/dtorespwithdata"
+	"github.com/mrexmelle/connect-emp/internal/profile"
 )
 
-type GetProfileResponseDto = dtorespwithdata.Class[career.Aggregate]
+type GetProfileResponseDto = dtorespwithdata.Class[profile.Aggregate]
 type GetCareerResponseDto = dtorespwithdata.Class[[]career.Aggregate]
